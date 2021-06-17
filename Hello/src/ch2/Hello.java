25d73cf0b81a008c0a00f22625ec4bf9f77635c5
TJ <TJ@DESKTOP-UOO4N74>
2021-06-17 21:40:07 +0900

    20210617처음커밋

---
+++ Hello/src/ch2/Hello.java
@@ -0,0 +1,11 @@
+package ch2;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+	}
+
+}
+
---
+++ Hello/src/ch33/VarTest1.java
@@ -0,0 +1,14 @@
+package ch33;
+
+public class VarTest1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a =10 , b;
+		b = a;
+		System.out.println("a="+a);
+		System.out.println("b="+b);
+		System.out.println(a+"+"+b+"="+(a+b));
+	}
+
+}
